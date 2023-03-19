55f4c2472ed14d8800e5def16ab35c0a5aa8d305
Dan Gialdini <dan.gialdini@minimaldigital.com.au>
2023-03-19 18:54:45 +1100

    Update the home page services section to use JSON data

---
+++ Client/Models/Services.cs
@@ -0,0 +1,10 @@
+namespace McNasGlass.Web.Models;
+
+public class Service
+{
+	#region Properties
+	public string Title { get; set; }
+	public string ImageUrl { get; set; }
+	public string Text { get; set; }
+	#endregion
+}
