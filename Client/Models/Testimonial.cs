4ec895e0b30f13219891cc236e4479a876964e0f
Dan Gialdini <dan.gialdini@minimaldigital.com.au>
2023-03-19 19:04:21 +1100

    Update the home page testimonials section to use JSON data

---
+++ Client/Models/Testimonial.cs
@@ -0,0 +1,10 @@
+namespace McNasGlass.Web.Models;
+
+public class Testimonial
+{
+	#region Properties
+	public string Title { get; set; }
+	public string Text { get; set; }
+	public string Author { get; set; }
+	#endregion
+}
