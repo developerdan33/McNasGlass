12642aedf513d554fa620e93a76683cd3e327c7e
Dan Gialdini <dan.gialdini@minimaldigital.com.au>
2023-03-19 18:46:44 +1100

    Update the home page about section to use JSON data

---
+++ Client/Models/About.cs
@@ -0,0 +1,10 @@
+namespace McNasGlass.Web.Models;
+
+public class About
+{
+	#region Properties
+	public string Title { get; set; }
+	public string Text { get; set; }
+	public string Icon { get; set; }
+	#endregion
+}
--- Client/Models/MenuItem.cs
+++ Client/Models/MenuItem.cs
@@ -2,6 +2,8 @@ namespace McNasGlass.Web.Models;
 
 public class MenuItem
 {
+	#region Properties
 	public string Name { get; set; }
 	public string Url { get; set; }
+	#endregion
 }
