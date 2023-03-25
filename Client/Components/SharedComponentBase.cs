1c5cc8df036ecab3ecbcbfecd1650efe456780d6
Dan Gialdini <dan.gialdini@minimaldigital.com.au>
2023-03-25 17:06:41 +1100

    Update the OG tags to use shared properties

--- Client/Components/SharedComponentBase.cs
+++ Client/Components/SharedComponentBase.cs
@@ -5,6 +5,7 @@ namespace McNasGlass.Web.Components;
 public class SharedComponentBase : LayoutComponentBase
 {
 	#region Properties
+	public string WebAddress { get; set; } = "https://www.mcnas.glass";
 	[Inject] public HttpClient HttpClient { get; set; }
 	#endregion
 }
